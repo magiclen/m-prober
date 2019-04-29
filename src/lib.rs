f0d379eedd18798fb2c50cb91b6aa53fa3a51757
Magic Len <len@magiclen.org>
2019-04-29 17:01:41 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -40,7 +40,7 @@ use network::NetworkWithSpeed;
 
 const DEFAULT_TERMINAL_WIDTH: usize = 64;
 const MIN_TERMINAL_WIDTH: usize = 60;
-const DEFAULT_INTERVAL: u64 = 333;
+const DEFAULT_INTERVAL: u64 = 333; // should be smaller than 1000 milliseconds
 
 const LABEL_COLOR: Color = Color::Rgb(0, 177, 177);
 const WHITE_COLOR: Color = Color::Rgb(219, 219, 219);
