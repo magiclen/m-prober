608f2439a4cc1ffb78e55e581d616c118310ddc2
Magic Len <len@magiclen.org>
2019-05-01 18:51:10 +0800

    implementing web

--- src/rocket_mounts/api/mod.rs
+++ src/rocket_mounts/api/mod.rs
@@ -61,7 +61,7 @@ impl SimpleAuthorization for Auth {
     }
 
     #[inline]
-    fn create_auth<S: Into<String>>(_key: Option<S>) -> Auth {
+    fn create_auth<S: AsRef<str>>(_key: Option<S>) -> Auth {
         Auth
     }
 }
