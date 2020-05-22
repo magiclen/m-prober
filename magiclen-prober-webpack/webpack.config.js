37b43793d98f478870170d791df43cc0d6198862
Magic Len <len@magiclen.org>
2020-05-22 22:27:52 +0800

    update front-end project

--- magiclen-prober-webpack/webpack.config.js
+++ magiclen-prober-webpack/webpack.config.js
@@ -50,9 +50,6 @@ module.exports = {
                 use: [
                     {
                         loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            hmr: process.env.NODE_ENV === 'development',
-                        },
                     },
                     'css-loader',
                     {
